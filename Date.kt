0e341f3e43279ffb79493cccf7d89666ae1ad0ae
JRA2880 <44944337+JRA2880@users.noreply.github.com>
2020-07-20 23:07:58 -0400

    Updated Kotlin Assignment Files to GitHub

---
+++ Date.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // Write your code below ☀️
+    var todaysDate: String = "07/15/2020"
+    println(todaysDate)
+    var currentWeather: String
+    currentWeather = "Sunny"
+    println(currentWeather)
+}
---
+++ Review.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // Write your code below
+    var favoriteSong: String ="Hey Jude"
+    var songLength: Double = 5.40
+    var listeningNow: Boolean = false;
+
+    println(favoriteSong)
+    println(songLength)
+    println(listeningNow)
+
+}
